a981eb4d5627724b66ba3358798c462eeda57a95
Manu <manu@Lucille>
2017-10-12 16:32:41 +0200

    creando el PPA con Python3

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='fondos',
+      version='0.0.1',
+      author='Manu Cogolludo',
+      author_email='makova65@gmail.com',
+      url='https://www.makova65.blogspot.com',
+      license='GPLv3',
+      data_files=[('/usr/share/icons/hicolor/scalable/apps',
+                  ['data/ubuntu.svg'])])
